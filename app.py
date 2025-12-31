6f85a0796441b6b19647a85abc63336b577c5181
Armel KI <kiarmelstephanenovak@gmail.com>
2025-12-31 11:05:45 +0100

    Add database connection function to app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
+import sqlite3
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'cle_secrete'
+
+def get_db_connection():
+    conn = sqlite3.connect('resolutions.db')
+    return conn
 
 from routes import *
 
